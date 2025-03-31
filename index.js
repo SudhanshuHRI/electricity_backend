6d15e86b33f0c44430981f0363caca843d0ff06a
sudhanshu <salil221254@gmail.com>
2025-03-31 17:30:03 +0530

    udpated

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express";
-import testRoute from "./routes/testRoute.js"
-
+import testRoute from "./routes/testRoute.js";
 
 const app = express();
 const port = 3500;
 
-app.use("/test",testRoute)
+app.use("/test", testRoute);
 
-app.listen(port,()=>{console.log("port is running on 3500")})
+app.listen(port, () => {
+  console.log("port is running on 3500");
+});
