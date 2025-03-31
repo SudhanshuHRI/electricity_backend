5fddf826d027dd1ba55cd731281122503cdde397
sudhanshu <salil221254@gmail.com>
2025-03-31 16:59:51 +0530

    updated

---
+++ controller/testController.js
@@ -0,0 +1,7 @@
+async function base(req, res) {
+  res.send({"message":"hello"});
+}
+
+export default {
+  base,
+};
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("electricity backend")
+import express from "express";
+import testRoute from "./routes/testRoute.js"
+
+
+const app = express();
+const port = 3500;
+
+app.use("/test",testRoute)
+
+app.listen(port,()=>{console.log("port is running on 3500")})
